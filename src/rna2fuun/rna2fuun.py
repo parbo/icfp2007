650c927d097027e707ea7fea1546db230a7561ee
parbo <parbo@localhost>
2007-07-20 12:22:05 +0000

    more stuff

--- src/rna2fuun/rna2fuun.py
+++ src/rna2fuun/rna2fuun.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import Image
 import ImageColor
 
@@ -30,33 +32,112 @@ def empty():
 
 bitmaps = [empty()]
 
+def splitrna(rna):
+    for i in xrange(len(rna), 7):
+        yield rna[i:i+7] 
+    
+def addColor(c):
+    bucket.insert(c, 0)
+    
+def getComponentIter(component):
+    for i in bucket:
+        yield i[component]
+    
+def average(component, default):
+    if bucket:
+        if component < 3:
+            return sum([c[component] for c in bucket if isinstance(c, tuple)])/len(bucket)
+        else:
+            return sum([c for c in bucket if isinstance(c, int)])/len(bucket)
+    else:
+        return default
+
+def currentPixel():
+    rc = average(bucket, 0, 0)
+    gc = average(bucket, 1, 0)
+    bc = average(bucket, 2, 0)
+    ac = average(bucket, 3, 255)
+    return (rc*ac/255, gc*ac/255, bc*ac/255, ac)
+
+def move(pos, d):
+    x, y = pos
+    if d == N:
+        return (x , (y - 1) % 600)
+    elif d == E:
+        return ((x + 1) % 600, y )
+    elif d == S:
+        return (x , (y + 1) % 600)
+    elif d == W:
+        return ((x - 1) % 600, y )
+    else:
+        raise "Unknown direction"
+
+def turnCounterClockwise(d):
+    return (d - 1) % 4
+
+def turnCounterClockwise(d):
+    return (d + 1) % 4
+
+def getPixel(p):
+    return bitmaps[0].getPixel(p)
+
+def setPixel(p):
+    bitmaps[0].setPixel(p, currentPixel())
+
+def setPixelVal(p, val):
+    bitmaps[0].setPixel(p, val)
+
+def line(start, stop):
+    x0, y0 = start
+    x1, y1 = stop
+    deltax = x1 - x0
+    deltay = y1 - y0
+    d = max(abs(deltax), abs(deltay))
+    
+    if (deltax * deltay) >= 0:
+        c = 1 
+    else:
+        c = 0
+        
+    x = x0 * d + (d - c)/2 
+    y = y0 * d + (d - c)/2
+    cp = currentPixel() 
+    for i in xrange(d):
+        setPixelVal(x/d, y/d, cp)
+        x = x + deltax
+        y = y + deltay
+    setPixelVal(x1, y1, cp)
+
 def build(filename):
     f = open(filename)
-    rna = f.read()
+    rna = splitrna(f.read())
     f.close()
     
+
+    
     for r in rna:
-182 case r is of the form
-183 ‘PIPIIIC’ ) addColor (black rgb)
-184 ‘PIPIIIP’ ) addColor (red rgb)
-185 ‘PIPIICC’ ) addColor (green rgb)
-186 ‘PIPIICF’ ) addColor (yellow rgb)
-187 ‘PIPIICP’ ) addColor (blue rgb)
-188 ‘PIPIIFC’ ) addColor (magenta rgb)
-189 ‘PIPIIFF’ ) addColor (cyan rgb)
-190 ‘PIPIIPC’ ) addColor (white rgb)
-191 ‘PIPIIPF’ ) addColor (transparent a)
-192 ‘PIPIIPP’ ) addColor (opaque a)
-193 ‘PIIPICP’ ) bucket   #
-194 ‘PIIIIIP’ ) position   move (position, dir)
-195 ‘PCCCCCP’ ) dir   turnCounterClockwise (dir)
-196 ‘PFFFFFP’ ) dir   turnClockwise (dir)
-197 ‘PCCIFFP’ ) mark   position
-198 ‘PFFICCP’ ) line (position, mark)
-199 ‘PIIPIIP’ ) tryfill ()
-200 ‘PCCPFFP’ ) addBitmap (transparentBitmap)
-201 ‘PFFPCCP’ ) compose ()
-202 ‘PFFICCF’ ) clip ()
+        if r == 
+case r is of the form
+183 â€˜PIPIIICâ€™ ) addColor (black rgb)
+184 â€˜PIPIIIPâ€™ ) addColor (red rgb)
+185 â€˜PIPIICCâ€™ ) addColor (green rgb)
+186 â€˜PIPIICFâ€™ ) addColor (yellow rgb)
+187 â€˜PIPIICPâ€™ ) addColor (blue rgb)
+188 â€˜PIPIIFCâ€™ ) addColor (magenta rgb)
+189 â€˜PIPIIFFâ€™ ) addColor (cyan rgb)
+190 â€˜PIPIIPCâ€™ ) addColor (white rgb)
+191 â€˜PIPIIPFâ€™ ) addColor (transparent a)
+192 â€˜PIPIIPPâ€™ ) addColor (opaque a)
+193 â€˜PIIPICPâ€™ ) bucket   #
+194 â€˜PIIIIIPâ€™ ) position   move (position, dir)
+195 â€˜PCCCCCPâ€™ ) dir   turnCounterClockwise (dir)
+196 â€˜PFFFFFPâ€™ ) dir   turnClockwise (dir)
+197 â€˜PCCIFFPâ€™ ) mark   position
+198 â€˜PFFICCPâ€™ ) line (position, mark)
+199 â€˜PIIPIIPâ€™ ) tryfill ()
+200 â€˜PCCPFFPâ€™ ) addBitmap (transparentBitmap)
+201 â€˜PFFPCCPâ€™ ) compose ()
+202 â€˜PFFICCFâ€™ ) clip ()
 203 anything else ) do nothing
 204 end case
 205 end foreach
@@ -71,7 +152,7 @@ the sequence of bitmaps. Each of the command groups is discussed in detail below
 4.3 Bucket commands
 For each of the eight predefined colors and the two predefined transparency values, there
 is an RNA command that prepends the Color to the bucket, using the procedure addColor in
-Figure 20. The instruction ‘PIIPICP’ empties the bucket.
+Figure 20. The instruction â€˜PIIPICPâ€™ empties the bucket.
 The bucket encodes information about a pixel, i.e., a current color and transparency
 value. The function currentPixel, also in Figure 20, can be used to determine this value of
 type Pixel.
