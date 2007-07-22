22fa08558dd556e4925ea9d91ab3f2ffb42ba123
dange <dange@localhost>
2007-07-22 21:39:04 +0000

    Added page prefix tool.

---
+++ prefix/pageprefix.py
@@ -0,0 +1,17 @@
+import sys
+
+def pageprefix(page):
+    bitlist = []
+    while (page > 0):
+        if (page % 2 == 0):
+            bitlist.append('C')
+        else:
+            bitlist.append('F')
+        page /= 2
+    pagetag = ''.join(bitlist)
+    return 'IIPIFFCPICFPPICIIC' + len(pagetag) * 'C' + 'IICIPPP' + pagetag + 'IIC'
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        page = int(sys.argv[1])
+        print pageprefix(page)
