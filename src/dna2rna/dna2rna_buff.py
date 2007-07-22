97c9ca73f774d3d134a123eb457bc7affa6ae098
dange <dange@localhost>
2007-07-22 12:33:02 +0000

    Added buffer implementation.

---
+++ src/dna2rna/dna2rna_buff.py
@@ -0,0 +1,350 @@
+import sys
+import time
+from rna2fuun.rna2fuun import commands as rnacommands
+from dnabuff import dnalist, NoMoreData
+
+# Extracts a pattern from the list 'dna', starting at position 'pos'.
+# Returns a tuple containing the pattern and a new position after the
+# consumed bases: (pattern, pos)
+# The 'rna' parameter is also modified during the process.
+def pattern(dna, rna):
+    p = []
+    lvl = 0
+    while True:
+##        print len(dna)
+        dnastr = ''.join(dna[0:3])
+##        print dnastr
+##        print "(",dnastr,")", len(dna)
+        if dnastr.startswith('C'):
+            dna.popfront()            
+            p.append('I')
+        elif dnastr.startswith('F'):
+            dna.popfront()
+            p.append('C')
+        elif dnastr.startswith('P'):
+            dna.popfront()
+            p.append('F')
+        elif dnastr.startswith('IC'):
+            dna.popfront(2)
+            p.append('P')
+        elif dnastr.startswith('IP'):
+            dna.popfront(2)
+            n = nat(dna)
+            p.append('!' + str(n))
+        elif dnastr.startswith('IF'):
+            dna.popfront(3) # NOTE: Three bases consumed here.
+            s = consts(dna)
+            p.append('?' + ''.join(s))
+        elif dnastr.startswith('IIP'):
+            dna.popfront(3)
+            lvl += 1
+            p.append('(')
+        elif dnastr.startswith('IIC') or dnastr.startswith('IIF'):
+            dna.popfront(3)
+            if lvl > 0:
+                lvl -= 1
+                p.append(')')
+            else:
+                return p
+        elif dnastr.startswith('III'):
+            # Add rna command.
+            rnacmd = dna[3:10]
+##            print rnacmd
+##            print len(dna)
+            dna.popfront(10)
+##            print len(dna)
+            if ''.join(rnacmd) not in rnacommands:
+##                print len(rna)
+                print 'Warning: Unknown RNA cmd: ' + ''.join(rnacmd)
+            rna.extend(rnacmd)
+        else:
+            # Exit
+            raise NoMoreData
+    return
+
+
+# Extracts a template from the list 'dna', starting at position 'pos'.
+# Returns a tuple containing the template and a new position after the
+# consumed bases: (template, pos)
+# The 'rna' parameter is also modified during the process.
+def template(dna, rna):
+    t = []
+    while True:
+        dnastr = ''.join(dna[0:3])
+        if dnastr.startswith('C'):
+            dna.popfront()
+            t.append('I')
+        elif dnastr.startswith('F'):
+            dna.popfront()
+            t.append('C')
+        elif dnastr.startswith('P'):
+            dna.popfront()
+            t.append('F')
+        elif dnastr.startswith('IC'):
+            dna.popfront(2)
+            t.append('P')
+        elif dnastr.startswith('IF') or dnastr.startswith('IP'):
+            dna.popfront(2)
+            l = nat(dna)
+            n = nat(dna)
+            t.append((l, n))
+        elif dnastr.startswith('IIP'):
+            dna.popfront(3)
+            n = nat(dna)
+            t.append(n)
+        elif dnastr.startswith('IIC') or dnastr.startswith('IIF'):
+            dna.popfront(3)      
+            return t
+        elif dnastr.startswith('III'):
+            # Add rna command.
+            rnacmd = dna[3:10]
+            dna.popfront(10)
+            if ''.join(rnacmd) not in rnacommands:
+                print 'Warning: Unknown RNA cmd: ' + ''.join(rnacmd)
+            rna.extend(rnacmd)
+        else:
+            # Exit
+            raise NoMoreData
+    return
+
+
+# Extracts a natural number from the list 'dna', starting at position 'pos'.
+# Returns a tuple containing the number and a new position after the
+# consumed bases: (number, pos)
+def nat(dna):
+    dnastr = ''.join(dna[0:1])
+    dna.popfront()
+    if (dnastr == 'P'):
+        return 0
+    elif (dnastr == 'I') or (dnastr == 'F'):
+        n = nat(dna)
+        return 2 * n
+    elif (dnastr == 'C'):
+        n = nat(dna)
+        return 2 * n + 1
+    else:
+        # Empty -> Exit
+        raise NoMoreData
+    
+    
+        
+# Extracts a base sequence from the list 'dna', starting at position 'pos'.
+# Returns a tuple containing the sequence and a new position after the
+# consumed bases: (sequence, pos)
+def consts(dna):
+    def constsrec(dna):
+        dnastr = ''.join(dna[0:2])
+        if dnastr.startswith('C'):
+            dna.popfront()
+            seq = constsrec(dna)
+            seq.append('I')
+            return seq
+        elif dnastr.startswith('F'):
+            dna.popfront()
+            seq = constsrec(dna)
+            seq.append('C')
+            return seq
+        elif dnastr.startswith('P'):
+            dna.popfront()
+            seq = constsrec(dna)
+            seq.append('F')
+            return seq
+        elif dnastr.startswith('IC'):
+            dna.popfront(2)
+            seq = constsrec(dna)
+            seq.append('P')
+            return seq
+        else:
+            return []
+            
+    seq = constsrec(dna)
+    seq.reverse()
+    return seq
+    
+# Modifies 'dna' by applying template 't' to matching items in pattern 'pat'.
+# The matching starts at position 'i'.
+def matchreplace(dna, pat, t):
+    e = []
+    c = []
+    i = 0
+ #   print ''.join(pat)
+ #   print t
+    for p in pat:
+        if p.startswith('!'):
+            n = int(p[1:])
+            i += n
+            if (i > len(dna)):
+                # Match failed.
+                #print 'Matched failed in !'
+                return
+        elif p.startswith('?'):
+            substr = p[1:]
+            n = dna.find(substr, i)
+            if n >= 0:
+                i = n + len(substr)
+            else:
+                # Match failed.
+                #print 'Matched failed in ?'
+                return
+        elif (p == '('):
+            c.append(i)
+        elif (p == ')'):
+  #          print c[-1], i
+            #e.append(slice(c.pop(),i))
+            e.append(dna.markblock(c.pop(), i))
+        else:
+            # Base
+            if (dna[i] == p):
+                i += 1
+            else:
+                # Match failed.
+                #print 'Matched failed in Base', p
+                return
+    dna.popfront(i)
+    replace(dna, t, e)
+    
+def replace(dna, tpl, e):
+    #print 'Replace ', dna, tpl, e 
+    r = []
+    s = []
+    for t in tpl:
+        #print 'replace:', t
+        if isinstance(t, int):
+            # |n|
+            if (t >= len(e)):
+                s.extend(asnat(0))
+            else:
+                #s.extend(asnat(len(e[t])))
+                s.extend(asnat(dna.blocksize(e[t])))
+        elif isinstance(t, tuple):
+            # n(l)
+            l, n = t
+            if (n >= len(e)):
+                s.extend(protect(l, []))
+            else:
+                if (l > 0):
+                    s.extend(protect(l, dna.readblock(e[n])))
+                else:
+                    if s:
+                        r.append(''.join(s))
+                        s = []
+                    r.append(e[n])
+        else:
+            # Base
+            s.append(t)
+            
+    if s:
+        r.append(''.join(s))
+            
+    for item in reversed(r):
+        dna.insertfront(item)
+    dna.arrange()
+    return
+    
+def protect(l, d):
+    while l:
+        d = quote(d)
+        l -= 1
+    return d
+        
+def quote(d):
+    nd = []    
+    for item in d:
+        if (item == 'I'):
+            nd.append('C')
+        elif (item == 'C'):
+            nd.append('F')
+        elif (item == 'F'):
+            nd.append('P')
+        else:
+            # P
+            nd.append('I')
+            nd.append('C')
+    return nd
+    
+def asnat(n):
+    d = []
+    while n > 0:
+        if (n % 2) == 0:
+            d.append('I')
+        else:
+            d.append('C')
+        n /= 2
+    d.append('P')
+    return d
+    
+def execute(dna, rna, progress = False):
+    n = 0
+    while True:
+        n += 1
+        try:
+            p = pattern(dna, rna)
+            #print p, dna
+            t = template(dna, rna)
+            #print t, dna
+            matchreplace(dna, p, t)
+            #print dna
+            if progress:
+                print 'Iterations: ' + str(n) + '   DNA remaining: ' + str(len(dna)), '   RNA commands: ' + str(len(rna) / 7)
+        except NoMoreData:
+            #print 'DNA remaining: ' + str(len(dna))
+            break
+            
+def step(dna, rna):
+    p = pattern(dna, rna)
+    t = template(dna, rna)
+    matchreplace(dna, p, t)
+    return
+
+if __name__ == '__main__':
+    prefix = ""
+    if len(sys.argv) > 3:
+        prefixfile = file(sys.argv[3], 'r')
+        prefix = prefixfile.read()
+        prefixfile.close()
+    if len(sys.argv) > 2:
+        dnafile = file(sys.argv[1], 'r')
+        dnastr = prefix + dnafile.read()
+        dna = dnalist(dnastr)
+        dnafile.close()
+        rna = []
+        starttime = time.time()
+        try:
+            execute(dna, rna, True)
+        except KeyboardInterrupt:
+            rnafile = file(sys.argv[2], 'w')
+            rnafile.write(''.join(rna))
+            rnafile.close()
+            sys.exit(1)
+        rnafile = file(sys.argv[2], 'w')
+        rnafile.write(''.join(rna))
+        rnafile.close()
+        endtime = time.time()
+        print ''
+        print 'Execution time: %.1f s' % (endtime - starttime)
+        
+    else:
+        # Run tests
+        print ''
+        print 'Test pattern function:'
+        print ''
+        for dnastr in ['CIIC', 'IIPIPICPIICICIIF']:
+            rna = []
+            dna = dnalist(dnastr)
+            p = []
+            try:
+                p = pattern(dna, rna)
+            except NoMoreData:
+                print 'DNA remaining: ' + str(len(dna))
+                pass
+            print dnastr + ' -> ' + ''.join(p)
+        print ''
+        print ''
+        print 'Test dna execution function:'
+        print ''
+        for dnastr in ['IIPIPICPIICICIIFICCIFPPIICCFPC', 'IIPIPICPIICICIIFICCIFCCCPPIICCFPC', 'IIPIPIICPIICIICCIICFCFC']:
+        #for dnastr in ['IIPIPICPIICICIIFICCIFPPIICCFPC']:
+            dna = dnalist(dnastr)
+            rna = []
+            step(dna, rna)
+            print dnastr + ' -> ' + str(dna)
---
+++ src/dna2rna/dnabuff.py
@@ -0,0 +1,207 @@
+BUFF_SIZE = 50000000
+INIT_OFS  = 10000000
+
+class NoMoreData(Exception):
+    pass
+    
+class dnalist:
+    def __init__(self, data):
+        self.buff = BUFF_SIZE * ['']
+        self.start = INIT_OFS
+        self.end = self.start + len(data)
+        self.buff[self.start:self.start + len(data)] = data
+        self.blocks = []
+        self.insert = []
+        return
+        
+    def __len__(self):
+        return self.end - self.start
+        
+    def __getitem__(self, key):
+        if isinstance(key, int):
+            return self.buff[self.start + key]
+        elif isinstance(key, slice):
+            return self.buff[self.start + key.start : self.start + key.stop]
+        raise TypeError
+        
+    def __str__(self):
+        if len(self) <= 50:
+            return ''.join(self.buff[self.start:self.end])
+        else:
+            return ''.join(self.buff[self.start:self.start+50]) + '...'
+        
+    def popfront(self, n = 1):
+        if (self.end - self.start) >= n:
+            self.start += n
+        else:
+            self.start = self.end
+            raise NoMoreData
+        return
+        
+    def insertfront(self, block):
+        #print 'dnalist.insertfront:', block
+        self.insert.append(block)
+        return
+        
+    def markblock(self, start, end):
+        # Store real indices.
+        blockid = len(self.blocks)
+        self.blocks.append((self.start + start, self.start + end))
+        return blockid
+        
+    def readblock(self, block):
+        start, end = self.blocks[block]
+        return self.buff[start:end]
+        
+    def blocksize(self, block):
+        start, end = self.blocks[block]
+        return end - start
+    
+    def _insertblockfront(self, block, blockdata):
+        data = None
+        if isinstance(block, int):
+            data = blockdata[block]
+        else:
+            data = block
+        self.buff[self.start - len(data) : self.start] = data
+        self.start -= len(data)
+        assert(self.start >= 0)
+        return
+        
+    def _insertblockend(self, block, blockdata):
+        data = None
+        if isinstance(block, int):
+            data = blockdata[block]
+        else:
+            data = block
+        self.buff[self.end : self.end + len(data)] = data
+        self.end += len(data)
+        assert(self.end < BUFF_SIZE)
+        return
+        
+    def arrange(self):
+        #print 'dnalist.arrange:'
+        #print '  self.blocks =', self.blocks
+        #print '  self.insert =', self.insert
+        maxblocksize = len(self)
+        maxblockix = -1
+        blocksize = 0
+        
+        # Find largest block (new blocks doesn't count).
+        for ix, block in enumerate(self.insert):
+            if isinstance(block, int):
+                blocksize = self.blocks[block][1] - self.blocks[block][0]                
+                if blocksize > maxblocksize:
+                    maxblocksize = blocksize
+                    maxblockix = ix
+                    
+        #print 'dnalist.arrange: maxblockix =', maxblockix
+           
+        # Extract data of referenced blocks.
+        blockdata = {}
+        for ix, block in enumerate(self.insert):
+            if isinstance(block, int) and (ix != maxblockix):
+                blockdata[block] = self.buff[self.blocks[block][0]:self.blocks[block][1]]
+                
+        if (maxblockix != -1):
+            # Store DNA
+            blockdata[-1] = self.buff[self.start:self.end]
+            
+        #print 'dnalist.arrange: blockdata =', blockdata
+            
+        # Largest block in fixed position (may be remaining DNA).
+        if (maxblockix == -1):
+            for block in self.insert:
+                self._insertblockfront(block, blockdata)
+        else:
+            # Set new start and end positions.
+            blockid = self.insert[maxblockix]
+            self.start, self.end = self.blocks[blockid]
+            
+            #print 'dnalist.arrange: self.start =', self.start
+            #print 'dnalist.arrange: self.end   =', self.end
+            
+            # Insert blocks.
+            for block in self.insert[maxblockix+1:]:
+                self._insertblockfront(block, blockdata)
+                
+            #print 'dnalist.arrange: dna =', self
+            
+            for block in reversed(self.insert[:maxblockix]):
+                self._insertblockend(block, blockdata)
+                
+            #print 'dnalist.arrange: dna =', self
+                
+            # Insert remaining DNA.
+            self._insertblockend(-1, blockdata)
+            
+            #print 'dnalist.arrange: dna =', self
+            
+        # Reset block lists.
+        self.blocks = []
+        self.insert = []
+        return
+        
+    def altfind(self, substr, startpos = 0):
+        subpos = 0
+        findpos = 0
+        i = startpos
+        while i < len(self):
+            if (self[i] == substr[subpos]):
+                if (subpos == 0):
+                    findpos = i 
+                subpos += 1
+                if (subpos == len(substr)):
+                    return findpos
+            elif (subpos > 0):
+                i = findpos
+                subpos = 0
+            i += 1
+        return -1
+        
+    def find(self, substr, startpos = 0):
+        s = ''.join(self[startpos:len(self)])
+        f = s.find(substr)
+        if f >= 0:
+            return f + startpos
+        else:
+            return f
+        
+if __name__ == '__main__':
+    # Test 1.
+    print ''
+    print 'Test 1'
+    print ''
+    d = dnalist('IICCCFFFPP')
+    print 'd =', d
+    print 'd[8] = ', d[8]
+    print 'd[3:7] =', d[3:7]
+    print 'd.popfront(3)'
+    d.popfront(3)
+    print 'd =', d
+    print "d.insertfront('ICFP')"
+    print 'd.arrange()'
+    d.insertfront('ICFP')
+    d.arrange()
+    print 'd =', d
+    print ''
+    print 'b = d.markblock(2, 4)'
+    print 'd.popfront(8)'
+    print 'd.insertfront(b)'
+    print "d.insertfront('PP')"
+    print 'd.arrange()'
+    b = d.markblock(2, 4)
+    d.popfront(8)
+    d.insertfront(b)
+    d.insertfront('PP')
+    d.arrange()
+    print 'd =', d
+    
+    # Test 2.
+    print ''
+    print 'Test 2'
+    print ''
+    d = dnalist('IICCCFFFPP')
+    print 'd =', d
+    print "d.find('CFF') =", d.find('CFF')
+    print "d.find('CFP') =", d.find('CFP')
