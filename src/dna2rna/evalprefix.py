31510fd2326b87919ff11bc3dff9a69e9d917800
dange <dange@localhost>
2007-07-23 00:23:35 +0000

    Added evalprefix.py

---
+++ src/dna2rna/evalprefix.py
@@ -0,0 +1,17 @@
+from dna2rna_ref import pattern, template
+import dnareflist
+import sys
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        dnafile = file(sys.argv[1], 'r')
+        dnastr = dnafile.read()
+        dna = dnareflist.DNAList()
+        dna.insertfront(dnareflist.DNARef(0, len(dnastr), dnastr))
+        dnafile.close()
+        rna = []
+        p = pattern(dna, rna)
+        t = template(dna, rna)
+        print 'pattern  =', p
+        print 'template =', t
+        
